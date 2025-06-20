ba12a5317b04910014508f52b4f66ab4e4d5a221
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2025-06-21 03:40:47 +0800

    initialize data ingestion and preprocessing

--- src/ingestion/process_data.py
+++ src/ingestion/process_data.py
@@ -1,3 +1,19 @@
+"""
+This script loads credit card transaction data,
+performs basic data cleaning (checking for nulls and dropping rows with missing values)
+"""
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv('data/raw/creditcard.csv')
+
+print(df.head())
+
+# Data Cleaning
+print(df.isnull().sum())
+df.dropna(inplace=True)
+
+# Feature Scaling
+scaler = StandardScaler()
+df['Amount'] = scaler.fit_transform(df[['Amount']])
+df['Class'] = df['Class'].astype('int')
