bf23e24e378c7f8622319f9f1979ef2f71a10779
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2025-06-23 05:37:53 +0800

    rename ingestion file

--- src/ingestion/ingest_kaggle_data.py
+++ src/ingestion/ingest_kaggle_data.py
@@ -1,41 +1,12 @@
-# import sys
-# import os
-
-# script_dir = os.path.dirname(__file__)
-# project_root = os.path.abspath(os.path.join(script_dir, '..', '..'))
-# sys.path.append(project_root)
-
-# import pandas as pd
-# from src.transformation.data_cleaning import DataCleaner
-# from src.transformation.feature_engineering import CreditCardFeatureProcessor
-
-# def main():
-#     df = pd.read_csv('data/raw/creditcard.csv')
-
-#     # Data Cleaning
-#     cleaner = DataCleaner()
-#     df = cleaner.transform(df)
-
-#     # Feature Scaling
-#     feature_processor = CreditCardFeatureProcessor()
-#     df = feature_processor.transform(df)
-
-#     df.reset_index(drop=True, inplace=True)
-
-#     # Save Data
-#     df.to_csv('data/processed/creditcard.csv', index=False)
-
-# main()
-
-# ============================================================================
-
 import os
 import logging
 import pandas as pd
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
 from src.ingestion.base_extractor import BaseExtractor
 from src.transformation.data_cleaning import DataCleaner
 from src.transformation.feature_engineering import CreditCardFeatureProcessor
+from config.logging_config import setup_logging
 
 load_dotenv()
 
@@ -66,6 +37,16 @@ class KaggleDataIngestion:
         self.input_path = input_path
         self.output_path = output_path
         self.extractor = KaggleDataExtractor(self.input_path, self.output_path)
+
+    def save_to_postgres(self, df: pd.DataFrame):
+        try:
+            engine = create_engine(os.getenv("DATABASE_URL"))
+
+            df.to_sql('credit_card_fraud', engine, index=False, if_exists='replace', method='multi')
+            logger.info("Data saved to PostgreSQL successfully.")
+        except Exception as e:
+            logger.error(f"Error while saving data to PostgreSQL: {e}")
+            raise e
 
     def run(self):
         try:
