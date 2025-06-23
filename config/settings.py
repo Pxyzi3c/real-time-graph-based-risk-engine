d60c84875e499c1ef59b8c043c575f948496656d
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2025-06-24 03:57:50 +0800

    connecting to docker postgres

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,16 @@
+import os
+
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Settings(BaseSettings):
     DATABASE_URL: str = "postgresql://{POSTGRES_USER}:{POSTGRES_PASS}@postgres_db:5432/{POSTGRES_DB}"
     API_KEY: str = "default_key"
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASS: str = os.getenv("POSTGRES_PASS")
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
 
     class Config:
         env_file = ".env"
--- src/common/db.py
+++ src/common/db.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import QueuePool
 from config.settings import settings
+from sqlalchemy.engine import URL
 
 _engine = None
 
@@ -18,6 +19,18 @@ def get_engine():
             pool_timeout=30,
             pool_recycle=3600,
         )
+
+        # _engine2 = create_engine(URL.create(
+        #     drivername='postgresql+psycopg2',
+        #     username=settings.POSTGRES_USER,
+        #     password=settings.POSTGRES_PASS,
+        #     host='postgres_db',
+        #     port=5432,
+        #     database=settings.POSTGRES_DB
+        # ), poolclass=QueuePool, pool_size=10, max_overflow=20, pool_timeout=30, pool_recycle=3600)
+        
+        # print(f"Engine: {_engine}\nEngine2: {_engine2}")
     return _engine
 
+# get_engine()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=get_engine())
