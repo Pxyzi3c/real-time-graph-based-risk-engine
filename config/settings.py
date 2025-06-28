784619af7d0889454add7783e815abebf0f6212b
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2025-06-28 19:35:47 +0800

    refactor settings.py

--- config/settings.py
+++ config/settings.py
@@ -6,11 +6,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://{POSTGRES_USER}:{POSTGRES_PASS}@postgres_db:5432/{POSTGRES_DB}"
-    API_KEY: str = "default_key"
+    DATABASE_URL: str = "postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASS}@localhost:6543/{POSTGRES_DB}"
     POSTGRES_USER: str = os.getenv("POSTGRES_USER")
     POSTGRES_PASS: str = os.getenv("POSTGRES_PASS")
     POSTGRES_DB: str = os.getenv("POSTGRES_DB")
+
+    DB_POOL_SIZE: int = 10
+    DB_MAX_OVERFLOW: int = 20
+    DB_POOL_TIMEOUT: int = 30
+    DB_POOL_RECYCLE: int = 3600
+
+    API_KEY: str = "default_key"
+
+    KAGGLE_INPUT_PATH: str = os.getenv("KAGGLE_INPUT_PATH", "data/raw/creditcard.csv")
+    KAGGLE_OUTPUT_PATH: str = os.getenv("KAGGLE_OUTPU_PATH", "data/processed/creditcard_processed.csv")
 
     class Config:
         env_file = ".env"
