3a63a61b3fa73cc093de5c92862c2daa64ebda96
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2025-07-19 12:00:00 +0800

    remove deprecated files

--- [deprecated] src/transformation/base_transformer.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-import pandas as pd
-
-class BaseTransformer(ABC):
-    @abstractmethod
-    def transform(self, df: pd.DataFrame, *args, **kwargs) -> pd.DataFrame:
-        """Abstract method to transform data."""
-        pass
